5104a04994125336a1e34b04e85518e871a5142e
Vpetros <rawkudodes@hotmail.com>
2025-10-15 20:09:28 +0300

    feat: add entity and invalid exception

---
+++ SchoolApp/Exceptions/InvalidRegistrationException.cs
@@ -0,0 +1,14 @@
+﻿namespace SchoolApp.Exceptions
+{
+    public class InvalidRegistrationException : Exception
+    {
+        public InvalidRegistrationException() { }
+        public InvalidRegistrationException(string message) : base(message) { }
+        public InvalidRegistrationException(string message, Exception inner) : base(message, inner) { }
+
+
+
+    }
+}
+ 
+
